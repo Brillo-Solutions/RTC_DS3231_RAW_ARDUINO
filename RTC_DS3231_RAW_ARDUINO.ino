4f020ddbadfcd0ab02e3ae88bd693cac1b4c9471
unknown <brillosolutions@gmail.com>
2020-08-03 20:39:37 +0530

    LCD DISPLAY - TIME, DATE, DAY-OF-WEEK

--- RTC_DS3231_RAW_ARDUINO.ino
+++ RTC_DS3231_RAW_ARDUINO.ino
@@ -1,67 +1,134 @@
 #include <Wire.h>
-#define DEV_ADDR 0x68
+#define DEV_ADDR_RTC 0x68 // I2C address of RTC - DS3231
+#define DEV_ADDR_LCD 0x3F // I2C address of PCF8574A extender
 
 byte mByte, mArr[7];
+byte backLight;
+const char *dayOfWeek[7] = {"MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"}; // Array of pointers containing days of week
 
 void setup() 
 {
   Wire.begin();
-  Serial.begin(115200);
+  setBackLight(true);
+  initDisplay();
+  setRtc();
 }
 
 void loop() 
 {
-  Wire.beginTransmission(DEV_ADDR);
-  Wire.write(0);  // set DS3231 register pointer to 00h
+  Wire.beginTransmission(DEV_ADDR_RTC);
+  Wire.write(0);  // Set base resiter address to 0h (Seconds)
   Wire.endTransmission();
-  Wire.requestFrom(DEV_ADDR, 7);  // seconds, minutes, hour, day, date, month, year
+  Wire.requestFrom(DEV_ADDR_RTC, 7);  // Seconds, Minutes, Hour, Day, Date, Month, Year
 
-  for (int k = 0; k < 7; k++)
+  for (int k = 0; k < 7; k++) // Reading time, day and date from DS3231
   {
     mByte = Wire.read();
-    mByte = (mByte / 16 * 10) + (mByte % 16);
     mArr[k] = mByte;
   }
 
-  Serial.println("Time:\t" + String(mArr[2] - 12) + ":" + String(mArr[1]) + ":" + String(mArr[0]));
-  Serial.println("Date:\t" + String(mArr[4]) + "/" + String(mArr[5]) + "/" + String(2000 + mArr[6]));
-  switch (mArr[3])
+  for (int m = 0, n = 12; m < 3; m++) // Showing time on 1st row of 16x2 LCD
   {
-    case 1:
-    {
-      Serial.println("Day:\tMonday\n");
-      break;
-    }
-    case 2:
-    {
-      Serial.println("Day:\tTuesday\n");
-      break;
-    }
-    case 3:
-    {
-      Serial.println("Day:\tWednesday\n");
-      break;
-    }
-    case 4:
-    {
-      Serial.println("Day:\tThursday\n");
-      break;
-    }
-    case 5:
-    {
-      Serial.println("Day:\tFriday\n");
-      break;
-    }
-    case 6:
-    {
-      Serial.println("Day:\tSaturday\n");
-      break;
-    }
-    case 7:
-    {
-      Serial.println("Day:\tSunday\n");
-      break;
-    }
+    showData(((mArr[m] & 0x0F) | 0x30), 1, n--);
+    showData(((mArr[m] >> 4) | 0x30), 1, n--);
+    if (n > 4)
+      showData(':', 1, n--);
   }
-  delay(1000);
+
+  for (int m = 4, n = 3; m < 7; m++)  // Showing date on 2nd row of 16x2 LCD
+  {
+    showData(((mArr[m] >> 4) | 0x30), 2, n++);
+    showData(((mArr[m] & 0x0F) | 0x30), 2, n++);
+    if (n < 11)
+      showData(':', 2, n++);
+  }
+
+  showData(dayOfWeek[mArr[3] - 1], 2, 12);
+}
+
+void sendToDisplay(byte mByte)  // Methods sending 8-bit data to PCF8574A I2C extender
+{
+  Wire.beginTransmission(DEV_ADDR_LCD);
+  Wire.write(mByte);
+  Wire.endTransmission();
+}
+
+void brkInstByte(byte mByte)
+{
+  byte nByte;
+  nByte = mByte & 0xF0;
+  sendToDisplay(nByte | (backLight | 0x04));
+  delay(1);
+  sendToDisplay(nByte | (backLight | 0x00));
+  nByte = ((mByte << 4) & 0xF0);
+  sendToDisplay(nByte | (backLight | 0x04));
+  delay(1);
+  sendToDisplay(nByte | (backLight | 0x00));
+}
+
+void brkDataByte(byte mByte)
+{
+  byte nByte;
+  nByte = mByte & 0xF0;
+  sendToDisplay(nByte | (backLight | 0x05));
+  delay(1);
+  sendToDisplay(nByte | (backLight | 0x01));
+  nByte = ((mByte << 4) & 0xF0);
+  sendToDisplay(nByte | (backLight | 0x05));
+  delay(1);
+  sendToDisplay(nByte | (backLight | 0x01));
+}
+
+void initDisplay()
+{
+  byte mArr[4] = {0x02, 0x28, 0x01, 0x0C};  // LCD 4-bit mode initialization commands
+  for (int k = 0; k < 4; k++)
+    brkInstByte(mArr[k]);  // Send 8-bit LCD commands in 4-bit mode
+
+  showData("--:--:--", 1, 5);
+  showData("--:--:-- ---", 2, 3);
+}
+
+void showData(const char *p, byte rn, byte cp)
+{
+  byte rowAddr;
+  
+  if (rn == 1)
+    rowAddr = 0x80; // First row of 16x2 LCD
+  else
+    rowAddr = 0xC0; // Second row of 16x2 LCD
+
+  brkInstByte(rowAddr + (cp - 1));  // Send 8-bit LCD commands in 4-bit mode
+    
+  while (*p != '\0')
+  {
+    brkDataByte(*p);  // Send 8-bit LCD data in 4-bit mode
+    p++;
+  }
+}
+
+void showData(byte mByte, byte rn, byte cp)
+{
+  byte rowAddr;
+  
+  if (rn == 1)
+    rowAddr = 0x80; // First row of 16x2 LCD
+  else
+    rowAddr = 0xC0; // Second row of 16x2 LCD
+
+  brkInstByte(rowAddr + (cp - 1));  // Send 8-bit LCD commands in 4-bit mode 
+  brkDataByte(mByte);  // Send 8-bit LCD data in 4-bit mode
+}
+
+void setBackLight(boolean mBool)
+{
+  if (mBool)
+    backLight = 0x08;  // Turn ON backlight of LCD
+  else
+    backLight = 0x00;  // Turn OFF backlight of LCD
+}
+
+void setRtc()
+{
+  
 }
